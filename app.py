44de6b97d8227d95822f14be0ba22ce4fdf407f2
lekshmicnair <65602900+lekshmicnair@users.noreply.github.com>
2020-10-04 08:50:57 -0400

    added stock data py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS, cross_origin
 import requests
 import json
 import pandas as pd
-import numpy as np
+import load_stock_data
 
 
 # Create an instance of Flask
@@ -22,6 +22,15 @@ app.config['CORS_ORIGINS'] = '*'
 app.config["MONGO_URI"] = "mongodb://localhost:27017/fortune500"
 mongo = PyMongo(app)
 
+#Getting latest stock data and loading to Mongo DB
+@app.before_first_request
+def stock_data():
+    try:
+        print("Data reader reading data from Yahoo finance...")
+        load_stock_data.get_stock_info()
+        print("Stock data succefully loaded to Mongo.")
+    except Exception as e:
+        print("Data load to Mongo failed. Please check the logs.")
 
 
 # Home Page Rendering 
@@ -41,7 +50,7 @@ def stock():
    return render_template('stock.html')
 
 
-# API endpoint for geomap data
+# route to get geomap data
 @app.route("/api/map" , methods=['GET'])
 def mapData():
  
@@ -50,7 +59,14 @@ def mapData():
     mapjson = json.loads(json_util.dumps(mapData))
     return jsonify(mapjson)
 
-
+# route to get stock data
+@app.route("/api/stock" , methods=['GET'])
+def stockData():
+ 
+    results = mongo.db.Walmart.find({})
+    stockData = pd.DataFrame(results)
+    stockjson = json.loads(json_util.dumps(stockData))
+    return jsonify(stockjson)
 
 
 if __name__ == "__main__":
---
+++ load_stock_data.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import pandas_datareader.data as pdr
+import datetime as dt
+import time
+import json
+from dateutil.relativedelta import relativedelta
+import pymongo
+from pymongo import MongoClient
+
+def get_stock_info():
+    #Get stock info from yahoo finance using datareader
+    start_date = dt.datetime.today() - relativedelta(years=1)
+    end_date = dt.datetime.today()
+
+
+    stock_data_1 = pdr.DataReader("WMT", 'yahoo', start_date, end_date)
+    stock_data_2 = pdr.DataReader("AMZN", 'yahoo', start_date, end_date)
+    stock_data_3 = pdr.DataReader("XOM", 'yahoo', start_date, end_date)
+    stock_data_4 = pdr.DataReader("AAPL", 'yahoo', start_date, end_date)
+    stock_data_5 = pdr.DataReader("CVS", 'yahoo', start_date, end_date)
+    
+    #sleep for 1 minute
+    time.sleep(60)
+
+    # Making Connection 
+    myclient = MongoClient("mongodb://localhost:27017/")  
+    # database  
+    db = myclient["fortune500"] 
+
+    ###Rank1- Insert stock data into Mongo DB
+    # Created or Switched to collection  
+    Collection = db["Walmart"] 
+    #Drop last collection
+    Collection.drop()
+    #Insert to Mongo
+    stock_data_1.reset_index(inplace=True)
+    data_dict = stock_data_1.to_dict("records")
+    Collection.insert_one({"index":"Date","data":data_dict})
+
+    ###Rank2- Insert stock data into Mongo DB
+    # Created or Switched to collection  
+    Collection = db["Amazon"] 
+    #Drop last collection
+    Collection.drop()
+    #Insert to Mongo
+    stock_data_2.reset_index(inplace=True)
+    data_dict = stock_data_2.to_dict("records")
+    Collection.insert_one({"index":"Date","data":data_dict})
+
+    ###Rank3- Insert stock data into Mongo DB
+    # Created or Switched to collection  
+    Collection = db["ExxonMobil"] 
+    #Drop last collection
+    Collection.drop()
+    #Insert to Mongo
+    stock_data_3.reset_index(inplace=True)
+    data_dict = stock_data_3.to_dict("records")
+    Collection.insert_one({"index":"Date","data":data_dict})
+
+    ###Rank4- Insert stock data into Mongo DB
+    # Created or Switched to collection  
+    Collection = db["Apple"] 
+    #Drop last collection
+    Collection.drop()
+    #Insert to Mongo
+    stock_data_4.reset_index(inplace=True)
+    data_dict = stock_data_4.to_dict("records")
+    Collection.insert_one({"index":"Date","data":data_dict})
+
+    ###Rank5- Insert stock data into Mongo DB
+    # Created or Switched to collection  
+    Collection = db["CVS"] 
+    #Drop last collection
+    Collection.drop()
+    #Insert to Mongo
+    stock_data_5.reset_index(inplace=True)
+    data_dict = stock_data_5.to_dict("records")
+    Collection.insert_one({"index":"Date","data":data_dict})
